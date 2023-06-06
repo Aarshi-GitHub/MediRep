e25e6242259561ccc0979de220f331b1c2fcb181
Aarshi Aggarwal <aarshiagg20@gmail.com>
2023-06-06 19:17:28 +0530

    Initial Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EC4D6D",
+        secondary: "#9CB2AA",
+        tertiary: "#B2D8CB",
+        bgprimary: "#FAFCFF",
+        bgsecondary: "#E0F2FE",
+        loginprimary : "#EC4D6D",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "ui-sans-serif"],
+        poppins: ["Poppins", "ui-sans-serif"],
+        lato: ["Lato", "SFMono-ui-monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
